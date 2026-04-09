92c08fd9db7162254c8547ba2a4f0920a06e3c94
edupoly <info.edupoly@gmail.com>
2026-04-09 09:32:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false })); //what this line does
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  app.get("/");
+  console.log("Request recieved");
+  res.send("Welcome to ExpressJS API endpoint creation");
+});
+
+app.get("/regStudent", function (req, res) {
+  console.log(req.query);
+  res.send("Registration is in Process... Wait for the reply");
+});
+
+app.post("/regStudent", function (req, res) {
+  console.log("req.params", req.params);
+  console.log("req.query", req.query);
+  console.log("req.body", req.body);
+  res.send(" lets understand the POST");
+});
+
+app.get(
+  "/add/:x/:y",
+  function (req, res, next) {
+    console.log("callback middleware called");
+    next();
+  },
+  function (req, res) {
+    console.log(req.params);
+    res.send(Number(req.params.x) + Number(req.params.y));
+  },
+);
+
+app.get("/sum", (req, res) => {
+  console.log(req.query);
+  res.send("Please wait...");
+});
+
+app.listen(3700, function () {
+  console.log("Server running on 3700");
+});
+
+// http://localhost:3700/sum?a=34&b=45
+// (req.query)
+
+// http://localhost:3700/add/89/45
+// (req.params)
